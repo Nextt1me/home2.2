e844c56015a0b5a285a6f4e29107a4ed303408b7
Vladimir <garulikvovka97@gmail.com>
2023-10-23 20:43:49 +0300

    initial commit

---
+++ src/main.java
@@ -0,0 +1,19 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Расчет бонусов зза пополнение кошелька");
+
+       int balance = 100;
+       int refill = 1_100;
+       int bonus;
+
+       if (refill > 1000) {
+           bonus = refill / 100;
+       }
+       else {
+           bonus = 0;
+       }
+        System.out.println("Количество бонусных рублей: " + bonus);
+
+        System.out.println("Итоговый расчет: " + (balance + refill + bonus));
+    }
+}
