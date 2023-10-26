4e652637c73142f9bcb89283695795c93edb22de
Vladimir <garulikvovka97@gmail.com>
2023-10-26 14:26:48 +0300

    fixed

--- src/main.java
+++ src/main.java
@@ -1,17 +1,16 @@
-public class main {
-    public static void main(String[] args) {
+public class Main {
+    public static void Main(String[] args) {
         System.out.println("Расчет бонусов зза пополнение кошелька");
 
-       int balance = 100;
-       int refill = 1_100;
-       int bonus;
+        int balance = 100;
+        int refill = 1_100;
+        int bonus;
 
-       if (refill > 1000) {
-           bonus = refill / 100;
-       }
-       else {
-           bonus = 0;
-       }
+        if (refill > 1000) {
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
+        }
         System.out.println("Количество бонусных рублей: " + bonus);
 
         System.out.println("Итоговый расчет: " + (balance + refill + bonus));
